3b9986744cc019b4faef96caeb09902b4e8cb6d6
Emmanuel T <emmanuel.tarimo@otapp.net>
2025-04-16 16:48:31 +0300

    fix: 'package:nested/nested.dart': Failed assertion: line 71 pos 16: 'children.isNotEmpty': is not true in providers

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,10 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+
+class DummyProvider extends ChangeNotifier {
+  String _dummyData = "Initial dummy data";
+
+  String get dummyData => _dummyData;
+
+  void updateData(String newData) {
+    _dummyData = newData;
+    notifyListeners();
+  }
+}
 
 /// Configure providers/dependencies for app functionalities
 ///
 List<SingleChildWidget> get providers {
   return [
     // e.g.
-    // Provider(create: (context) => SomeProvider());
+    // ChangeNotifierProvider(create: (context) => SomeProvider());
+    ChangeNotifierProvider<DummyProvider>(create: (context) => DummyProvider()),
   ];
 }
